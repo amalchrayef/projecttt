0352da56a019fabf8028ed4604a054f069270950
amalchrayef <chrayefa@gmail.com>
2021-07-05 22:13:48 +0200

    config stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,1 +1,7 @@
 #Stripe.api_key = Rails.application.credentials.stripe[:STRIPE_SECRET_KEY]
+
+Rails.configuration.stripe = {
+     
+    :secret_key => Rails.application.credentials.stripe[:STRIPE_SECRET_KEY]    
+  }    
+  Stripe.api_key = Rails.configuration.stripe[:secret_key]
