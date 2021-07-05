45084f2ca4b15eca8ad29cb66afe0344d2031bc3
amalchrayef <chrayefa@gmail.com>
2021-07-06 01:11:53 +0200

    commit 2 fb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -329,7 +329,7 @@ Devise.setup do |config|
     when "production"
 
   
-      config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY'],  callback_url: "https://freeworkk.herokuapp.com/dashboard",
+      config.omniauth :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET_KEY'],  callback_url: "https://freeworkk.herokuapp.com/auth/facebook",
         scope: 'email',
                     info_fields: 'email,name',image: 'large'
    end
