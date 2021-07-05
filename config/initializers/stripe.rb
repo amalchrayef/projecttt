385cc2489b01d902713d917ae954d469c59eb297
amalchrayef <chrayefa@gmail.com>
2021-07-05 22:19:53 +0200

    commit stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,15 @@
 #Stripe.api_key = Rails.application.credentials.stripe[:STRIPE_SECRET_KEY]
 
-Rails.configuration.stripe = {
+#Rails.configuration.stripe = {
      
-    :secret_key => Rails.application.credentials.stripe[:STRIPE_SECRET_KEY]    
-  }    
+ #   :secret_key => Rails.application.credentials.stripe[:STRIPE_SECRET_KEY]    
+  #}    
+  #Stripe.api_key = Rails.configuration.stripe[:secret_key]
+
+  Rails.configuration.stripe = {
+  
+    :secret_key      => 'sk_test_51J5JTTLcOfPRsqGj5MBumE1lMOLob0T0hmtbm1dtweOBEX1H4svN5LX2PF6JLBCsCMvBvui3COs9l5D1MTZYcH9x000B3LQ0FW'
+  
+  }
+  
   Stripe.api_key = Rails.configuration.stripe[:secret_key]
