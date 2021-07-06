bf32ca9a95d642900069b76245eb74c1c1f93f45
amalchrayef <chrayefa@gmail.com>
2021-07-06 03:45:53 +0200

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,8 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :production
+  #config.active_storage.service = :production
+  config.active_storage.service = :cloudinary
   #ENV['FACEBOOK_ID']="277996777451532"
   #ENV['FACEBOOK_SECRET_KEY']="790431cf2a31e7b0b96b80bc03a2780a"
   # Mount Action Cable outside main process or domain.
